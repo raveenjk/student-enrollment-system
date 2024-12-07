e002899354bc6eb5868e98bfda94a9e38981bc08
raveenjk <raveenmadhawa48@gmail.com>
2024-12-07 22:06:09 +0530

    Create assignment_view.py

---
+++ views/assignment_view.py
@@ -0,0 +1,5 @@
+
+class AssignmentView:
+    @staticmethod
+    def display_assignment(assignment):
+        print(f"Assignment ID: {assignment.assignment_id}, Title: {assignment.title}, Description: {assignment.description}")
