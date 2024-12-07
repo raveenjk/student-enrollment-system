011670e4beec537cc63277f3ce9f918909d665aa
raveenjk <raveenmadhawa48@gmail.com>
2024-12-07 22:04:08 +0530

    Create assignment.py

---
+++ models/assignment.py
@@ -0,0 +1,9 @@
+
+class Assignment:
+    def __init__(self, assignment_id: int, title: str, description: str):
+        self.assignment_id = assignment_id
+        self.title = title
+        self.description = description
+
+    def submit(self, student):
+        pass
